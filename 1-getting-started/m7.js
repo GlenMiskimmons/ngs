d9b3ac71d2a1600bda87c1f33405749d5d33c691
GlenMiskimmons <42587792+GlenMiskimmons@users.noreply.github.com>
2021-01-29 16:09:32 +0000

    Module 03 Getting Started with Node
    Node's REPL Mode

---
+++ 1-getting-started/m7.js
@@ -0,0 +1,21 @@
+Math.random
+Math.random()
+Math.random()
+let answer = 42
+answer
+3 === '3'
+3 == '3'
+function today() {
+return new Date();
+}
+today()
+function add(x, y) {
+  return x = y;
+}
+
+function random() {
+  return Math.random()
+}
+
+random()
+add(1, 2)
