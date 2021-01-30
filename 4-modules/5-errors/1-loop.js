bc5f7dfb86519df0b62c62e6c86ecb8537972548
GlenMiskimmons <42587792+GlenMiskimmons@users.noreply.github.com>
2021-01-30 16:29:22 +0000

    Module 06 Modules and Concurrency
    Error vs Exceptions

--- 4-modules/5-errors/1-loop.js
+++ 4-modules/5-errors/1-loop.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const fs = require('fs');
 
-const files = ['.bash_profile', '.npmrc'];
+const files = ['./1-loop.js', './zzz.js', './2-try.js'];
 
 files.forEach(file => {
-  const filePath = path.resolve(process.env.HOME, file);
+  const filePath = path.resolve(file);
   const data = fs.readFileSync(filePath);
   console.log('File data is', data);
 });
--- 4-modules/5-errors/3-throw.js
+++ 4-modules/5-errors/3-throw.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const fs = require('fs');
 
-const files = ['.bash_profile', 'kjkjhh', '.npmrc'];
+const files = ['./1-loop.js', './zzz.js', './2-try.js'];
 
 files.forEach(file => {
   try {
-    const filePath = path.resolve(process.env.HOME, file);
-    const data = fs.readFileSync(filePath);
+    const filePath = path.resolve(file);
+    const data = fs.readFileSync(filePath, 'utf-42');
     console.log('File data is', data);
   } catch (err) {
     if (err.code === 'ENOENT') {
